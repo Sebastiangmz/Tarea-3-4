233ec4984b20a80dd077a6eb39ea71d68db518be
nachomarr <ignaciomarquezrobles@outlook.com>
2024-10-02 23:13:37 -0600

    modify script.js add resta

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 let num_a = 5,
 num_b = 10;
-let res = num_a + num_b;
+let sum = num_a + num_b;
+let res = num_a - num_b;
 console.log('Resultado', res);
+console.log('Resultado', sum);
 alert('Práctica 1');
